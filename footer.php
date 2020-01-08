5cb6b28c315448334181f6b3adb97b3cd0a0bf8d
Ashbin Thapa <thapasmit82@gmail.com>
2020-01-08 11:00:33 +0545

    first commit

---
+++ footer.php
@@ -0,0 +1,24 @@
+<div class="footer">
+    <div class="first-row-footer">
+        <div class="first-column">
+            <h3>other links</h3>
+            <ul>
+                <li>हाम्रो बारे</li>
+                <li>ADVERTISE WITH US</li>
+                <li>CONTACT US</li>
+                <li>PRIVACY POLICY</li>
+                <li>TERMS OF USE</li>
+            </ul>
+        </div>
+        <div class="second-column">
+            <h3>social links</h3>
+            <img src="https://img.icons8.com/cute-clipart/128/000000/facebook-new.png">
+            <img src="https://img.icons8.com/cute-clipart/128/000000/twitter.png">
+            <img src="https://img.icons8.com/cute-clipart/128/000000/youtube.png">
+        </div>
+        <div class="third-column"></div>
+    </div>
+    <div class="second-row-footer">
+        <h3>&copy; All Right Reserverd</h3>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <div class="head-wrapper">
+        <div class="advertisement">
+            <img src="https://www.genesiswtech.com/wp-content/uploads/2020/01/NMB-Monk_Nari-1140x90_1.gif">
+        </div>
+        <div class="site-main-title">
+            <img src="https://img.icons8.com/color/48/000000/news.png">
+        </div>
+        <header class="header">
+            <a class="logo">online portal</a>
+            <input class="menu-btn" type="checkbox" id="menu-btn" />
+            <label for="menu-btn" class="menu-icon">
+            <span class="nav-icon"></span>
+        </label>
+            <ul class="menu">
+                <li><a>home</a></li>
+                <li><a>politics</a></li>
+                <li><a>business</a></li>
+                <li><a>interview</a></li>
+                <li><a>lifestyle</a></li>
+                <li><a>travel</a></li>
+            </ul>
+        </header>
+    </div>
+    <?php include('footer.php'); ?>
+</body>
+
+</html>
